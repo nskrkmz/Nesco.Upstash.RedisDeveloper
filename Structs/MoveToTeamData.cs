00b515f5badeb553816aac200bfa48e4748a0374
nskrkmz <aeneskrkmz@gmail.com>
2024-03-07 14:17:02 +0300

    implement IUpstashRedisDeveloperService interface

---
+++ Structs/MoveToTeamData.cs
@@ -0,0 +1,14 @@
+﻿namespace Nesco.Upstash.RedisDeveloper
+{
+    internal struct MoveToTeamData
+    {
+        /// <summary>
+        /// The ID of the target team
+        /// </summary>
+        public string team_id;
+        /// <summary>
+        /// The ID of the database to be moved
+        /// </summary>
+        public string database_id;
+    }
+}
