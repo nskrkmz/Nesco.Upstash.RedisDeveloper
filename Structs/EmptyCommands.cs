ab0345938ac7090891298f79fe0e34965e416d67
nskrkmz <aeneskrkmz@gmail.com>
2024-03-09 16:59:50 +0300

    add EmptyCommands struct

---
+++ Structs/EmptyCommands.cs
@@ -0,0 +1,9 @@
+﻿namespace Nesco.Upstash.RedisDeveloper
+{
+#pragma warning disable IDE1006 // Naming Styles
+    public class EmptyCommands
+    {
+        public string? x { get; set; }
+        public int y { get; set; }
+    }
+}
